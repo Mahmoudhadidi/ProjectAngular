89cbc5cd0a43b0f45f68077b1e92fddfd01ba7e9
hadidi <mahmoudhadidi2017@gmail.com>
2020-11-22 00:54:32 +0100

    Liste des tâches + formular ajout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TasksComponent } from './tasks/tasks.component';
 
+import{HttpClientModule} from'@angular/common/http';
+import{FormsModule} from'@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
