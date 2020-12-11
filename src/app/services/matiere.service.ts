7f89886bb1c2d4761cd4ca393ab70112338a3cd0
hadidi <mahmoudhadidi2017@gmail.com>
2020-12-11 20:49:17 +0100

    creation service Matiere (ajout,affiche, supprime et modife)

---
+++ src/app/services/matiere.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Matiere } from '../models/matiere';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatiereService {
+  apiurl="http://localhost:3000/matiere";
+  
+  constructor(private http: HttpClient) { }
+
+  findAll(){
+    return this.http.get<Matiere[]>(this.apiurl);
+    }
+  findAllMatiere(){
+    return this.http.get<Matiere[]>(this.apiurl);
+  }
+  delete(id){
+    return this.http.delete(this.apiurl+'/'+id)
+  }
+  persist(matiere){
+    return this.http.post<Matiere>(this.apiurl,matiere);
+  }
+  
+  update(matiere){
+    return this.http.put(this.apiurl+'/'+matiere.id,matiere)
+  }
+}
+
